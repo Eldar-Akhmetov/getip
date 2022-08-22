9076f0d866280e6f41e36605fa6d69392710c4b6
Eldar Akhmetov <goodtrade02@gmail.com>
2022-08-22 21:30:36 +0700

    Fixed the error

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 #
 # Made by Eldar Akhmetov Copyleft (c) 2022
 # ---------------------------------------------------------------------
-from app import application, db
+from app import app, db
 import view
 from db_manager import DB_Manager
 
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
