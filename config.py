ec555266db6e48bfd31c0429930939a25c2eb969
Eldar Akhmetov <goodtrade02@gmail.com>
2022-08-22 21:46:21 +0700

    Test version

--- config.py
+++ config.py
@@ -9,13 +9,14 @@ db_user = os.environ['RDS_USERNAME']
 db_password = os.environ['RDS_PASSWORD']
 db_hostname = os.environ['RDS_HOSTNAME']
 db_port = os.environ['RDS_PORT']
-            
+
+
 class Configuration(object):
     # Включить или отключить режим отладки
     DEBUG = True
-    
+
     # Включение и отключение функции отслеживания изменения объектов
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
     # Параметры для подключения к базе данных
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_hostname}:{db_port}/{db_name}'
